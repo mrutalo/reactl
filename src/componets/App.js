d24594b0fd335c7281e32c66605c7bddde5d25ff
mrutalo <michael.rutalo@gmail.com>
2024-06-28 20:10:36 +0300

    App starting fresh

---
+++ src/componets/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import Second from "./second.js";
+
+function App() {
+  return (
+    <div>
+      <h1>Hello React</h1>
+      <Second />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/componets/second.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Second() {
+  return <h1>hello</h1>;
+}
+
+export default Second;
